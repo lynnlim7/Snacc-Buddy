5765c095762ea3578f93687047818b273592ef39
lynnlim99 <lynnlim99@hotmail.com>
2026-05-20 12:28:25 +0800

    resolve landing page hydration issue

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${baloo.variable} ${nunito.variable}`}>
-      <body className="min-h-screen">{children}</body>
+      <body className="min-h-screen" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
