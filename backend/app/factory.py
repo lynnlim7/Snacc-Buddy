6f99f583f5f4874509d3ba94b0ce7c38cb7a1b64
lynnlim99 <lynnlim99@hotmail.com>
2026-06-24 17:46:01 +0800

    add email verification router

--- backend/app/factory.py
+++ backend/app/factory.py
@@ -93,6 +93,7 @@ def create_app() -> FastAPI:
     )
 
     app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
+    app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
     app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix="/auth", tags=["auth"])
     app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
     app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
